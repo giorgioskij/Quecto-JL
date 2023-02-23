1c3bb4788f52580de284670bde28204deb92bfbf
antoandgar <antonioandgargiulo@gmail.com>
2023-02-23 21:56:23 +0100

    matte and hopefully a polished metal

--- jtrace.jl
+++ jtrace.jl
@@ -20,7 +20,7 @@ const global masterBvhDepth = 19
 
 # main entry point to the program
 function run(
-    shader::Function = shaderEyelight,
+    shader::Function = shaderMaterial,
     width = 1920,
     height = 1080,
     numSamples = 2,
@@ -67,7 +67,8 @@ function traceSamples(shader, image, scene, imwidth, imheight, numSamples, bvh)
                 )
 
                 weight::Float32 = 1 / s
-                image[j, i] = linInterp(image[j, i], color, weight)
+                image[j, i] =
+                    clamp.(linInterp(image[j, i], color, weight), 0.0f0, 1.0f0)
             end
         end
     end
@@ -150,11 +151,206 @@ function shaderEyelight(scene::Scene, ray::Ray, bvh::SceneBvh)::SVec3f
 
     outgoing = -ray.direction
 
-    color = material.color
     # radiance = 0.5 .* (normal .+ 1) .* color
-    radiance::SVec3f = abs(dot(normal, outgoing)) .* color
+    radiance::SVec3f = abs(dot(normal, outgoing)) .* material.color
 
     return radiance
+end
+
+function shaderMaterial(scene::Scene, ray::Ray, bvh::SceneBvh)::SVec3f
+    # intersection::Intersection = intersectScene(ray, scene)
+    radiance = SVec3f(0, 0, 0)
+    maxBounce = 4
+
+    for b = 1:maxBounce
+        intersection::Intersection = intersectScene(ray, scene, bvh, false)
+
+        if !intersection.hit
+            radiance = evalEnvironment(scene, ray.direction)
+            # radiance = SVec3f(0, 0, 0)
+            return radiance
+        end
+
+        # compute normal of the point hit
+        instance::Instance = scene.instances[intersection.instanceIndex]
+        frame::Frame = instance.frame
+        shape::Shape = scene.shapes[instance.shapeIndex]
+        material::Material = scene.materials[instance.materialIndex]
+
+        radiance = material.emission
+        normal = evalNormal(shape, intersection, frame)
+
+        # do positions
+        #position = evalPosition(shape, intersection, frame)
+        outgoing = -ray.direction
+
+        if material.type == "matte"
+            incoming = sampleHemisphereCos(normal)
+            if dot(normal, incoming) * dot(normal, outgoing) <= 0
+                return SVec3f(0, 0, 0)
+            end
+            radiance += material.color / pi .* abs(dot(normal, outgoing))
+        elseif material.type == "reflective"
+            incoming = reflect(outgoing, normal)
+            if dot(normal, incoming) * dot(normal, outgoing) <= 0
+                return SVec3f(0, 0, 0)
+            end
+            up_normal = dot(normal, outgoing) <= 0 ? -normal : normal
+            halfway = normalize(incoming + outgoing)
+            F = fresnelConductor(
+                reflectivityToEta(material.color),
+                SVec3f(0, 0, 0),
+                halfway,
+                incoming,
+            )
+            D = microfacetDistribution(material.roughness, up_normal, halfway)
+            G = microfacetShadowing(
+                material.roughness,
+                up_normal,
+                halfway,
+                outgoing,
+                incoming,
+            )
+            radiance +=
+                material.color .* F .* D .* G ./
+                (4 .* dot(up_normal, outgoing) .* dot(up_normal, incoming)) .*
+                abs(dot(up_normal, incoming))
+
+            # elseif material.roughness == 0
+
+        else
+            radiance = abs(dot(normal, outgoing)) .* material.color
+        end
+
+        # radiance = 0.5 .* (normal .+ 1) .* color
+        #radiance::SVec3f = abs(dot(normal, outgoing)) .* radiance
+    end
+    return radiance
+end
+
+function reflectivityToEta(color::SVec3f)::SVec3f
+    reflectivity::SVec3f = clamp.(color, 0.0f0, 1.0f0)
+    return (1.0f0 .+ sqrt.(reflectivity)) ./ (1.0f0 .- sqrt.(reflectivity))
+end
+
+function reflect(w::SVec3f, n::SVec3f)::SVec3f
+    return w + 2.0f0 * dot(w, n) * n
+end
+
+@inline function fresnelConductor(
+    eta::SVec3f,
+    etak::SVec3f,
+    normal::SVec3f,
+    outgoing::SVec3f,
+)::SVec3f
+    cosw::Float32 = dot(normal, outgoing)
+    if cosw <= 0
+        return SVec3f(0, 0, 0)
+    end
+    cosw = clamp(cosw, -1.0f0, 1.0f0)
+    cos2::Float32 = cosw * cosw
+    sin2::Float32 = clamp(1 - cos2, 0.0f0, 1.0f0)
+    eta2::SVec3f = eta .* eta
+    etak2::SVec3f = etak .* etak
+
+    t0::SVec3f = eta2 .- etak2 .- sin2
+    a2plusb2::SVec3f = sqrt.(t0 .* t0 .+ 4.0f0 .* eta2 .* etak2)
+    t1::SVec3f = a2plusb2 .+ cos2
+    a::SVec3f = sqrt.((a2plusb2 .+ t0) ./ 2.0f0)
+    t2::SVec3f = 2.0f0 .* a .* cosw
+    rs::SVec3f = (t1 .- t2) ./ (t1 .+ t2)
+
+    t3::SVec3f = cos2 .* a2plusb2 .+ sin2 .* sin2
+    t4::SVec3f = t2 .* sin2
+    rp::SVec3f = rs .* (t3 .- t4) ./ (t3 .+ t4)
+
+    return (rp .+ rs) ./ 2.0f0
+end
+
+@inline function microfacetDistribution(
+    roughness::Float32,
+    normal::SVec3f,
+    halfway::SVec3f,
+    ggx::Bool = true,
+)::Float32
+    cosine = dot(normal, halfway)
+    if cosine <= 0
+        return 0.0f0
+    end
+    roughness2 = roughness * roughness
+    cosine2 = cosine * cosine
+    if ggx
+        # maybe this is wrong because of the + 1 with no parentesis?
+        return roughness2 / ((pi * (cosine2 * roughness2 + 1 - cosine2)^2))
+    else
+        return exp((cosine2 - 1) / (roughness2 * cosine2)) /
+               (pi * roughness2 * cosine2 * cosine2)
+    end
+end
+
+@inline function microfacetShadowing(
+    roughness::Float32,
+    normal::SVec3f,
+    halfway::SVec3f,
+    outgoing::SVec3f,
+    incoming::SVec3f,
+    ggx::Bool = true,
+)
+    return microfacetShadowing1(roughness, normal, halfway, outgoing, ggx) *
+           microfacetShadowing1(roughness, normal, halfway, incoming, ggx)
+end
+
+@inline function microfacetShadowing1(
+    roughness::Float32,
+    normal::SVec3f,
+    halfway::SVec3f,
+    direction::SVec3f,
+    ggx::Bool,
+)
+    cosine = dot(normal, direction)
+    cosineh = dot(halfway, direction)
+    if cosine * cosineh <= 0
+        return 0.0f0
+    end
+    roughness2 = roughness * roughness
+    cosine2 = cosine * cosine
+    if ggx
+        return 2 * abs(cosine) /
+               (abs(cosine) + sqrt(cosine2 - roughness2 * cosine2 + roughness2))
+    else
+        ci = abs(cosine) / (roughness * sqrt(1 - cosine2))
+        return ci < 1.6f0 ?
+               (3.535f0 * ci + 2.181f0 * ci * ci) /
+               (1.0f0 + 2.276f0 * ci + 2.577f0 * ci * ci) : 1.0f0
+    end
+end
+
+function sampleHemisphere(normal::SVec3f)::SVec3f
+    ruvx = rand()
+    z = rand()  # ruvy
+    r = sqrt(clamp(1 - z * z, 0.0f0, 1.0f0))
+    phi = 2 * pi * ruvx
+    localDirection = SVec3f(r * cos(phi), r * sin(phi), z)
+    return transformDirection(basisFromz(normal), localDirection)
+end
+
+function sampleHemisphereCos(normal::SVec3f)::SVec3f
+    ruvx = rand()
+    z = sqrt(rand())  # ruvy
+    r = sqrt(clamp(1 - z * z, 0.0f0, 1.0f0))
+    phi = 2 * pi * ruvx
+    localDirection = SVec3f(r * cos(phi), r * sin(phi), z)
+    return transformDirection(basisFromz(normal), localDirection)
+end
+
+function basisFromz(v::SVec3f)
+    z = normalize(v)
+    sign = copysign(1.0f0, z.z)
+    a = -1.0f0 / (sign + z.z)
+    b = z.x * z.y * a
+    x = SVec3f(1.0f0 + sign * z.x * z.x * a, sign * b, -sign * z.x)
+    y = SVec3f(b, sign + z.y * z.y * a, -z.y)
+    return Frame(x, y, z)
 end
 
 function intersectTriangle(
--- main.jl
+++ main.jl
@@ -10,11 +10,12 @@ cd(baseDir)
 
 # const scenePath = joinpath(baseDir, "03_texture/texture.json")
 #const scenePath = joinpath(baseDir, "02_matte/matte.json")
-# const scenePath = joinpath(baseDir, "02_matte/bunny.json")
+#const scenePath = joinpath(baseDir, "02_matte/bunny.json")
 # const scenePath = joinpath(baseDir, "04_envlight/envlight.json")
-const scenePath = joinpath(baseDir, "12_ecosys/ecosys.json")
+# const scenePath = joinpath(baseDir, "12_ecosys/ecosys.json")
+const scenePath = joinpath(baseDir, "materials1/materials1.json")
 
 # set shader to use
-# shader = shaderNormal
-shader = shaderEyelight
+# shader = shaderEyelight
+shader = shaderMaterial
 #run(shader, 1920, 1080, 2)
