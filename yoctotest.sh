78475f483cf4197853464e52960061bb162fee3d
Giorgioskij <giorgiopuntostrano@gmail.com>
2023-02-26 20:15:45 +0100

    edit authors

--- yoctotest.sh
+++ yoctotest.sh
@@ -1,9 +1,8 @@
 #! /bin/zsh
 
 ~/dev/uni/computer-graphics/masters/yocto-gl/bin/ytrace \
-    --scene ~/dev/uni/computer-graphics/masters/Jtrace/materials1/materials1.json \
-    --samples 64 \
-    --resolution 1280 \
+    --scene ~/dev/uni/computer-graphics/masters/Jtrace/03_texture/texture.json \
+    --samples 2 \
+    --resolution 1920 \
     --output ~/dev/uni/computer-graphics/masters/Jtrace/out/yocto.png \
-    --sampler naive \
-    --bounces 32
+    --sampler eyelight
