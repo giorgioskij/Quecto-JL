b0088902f07ff61eaea890e9288f7794afb0b99b
Giorgioskij <giorgiopuntostrano@gmail.com>
2023-02-26 19:30:37 +0100

    small improvements

--- yoctotest.sh
+++ yoctotest.sh
@@ -1,7 +1,9 @@
 #! /bin/zsh
 
-../yocto-gl/bin/ytrace \
-    --scene 04_envlight/envlight.json \
-    --samples 100 \
-    --output out/yocto.png \
-    --sampler eyelight 
+~/dev/uni/computer-graphics/masters/yocto-gl/bin/ytrace \
+    --scene ~/dev/uni/computer-graphics/masters/Jtrace/materials1/materials1.json \
+    --samples 64 \
+    --resolution 1280 \
+    --output ~/dev/uni/computer-graphics/masters/Jtrace/out/yocto.png \
+    --sampler naive \
+    --bounces 32
